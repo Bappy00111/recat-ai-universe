0409f32a4a987d7ea56a3e38dfd966e95f491bae
Bappy <bappymodok0011@gmail.com>
2023-11-21 18:40:50 +0600

    modal data added

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -1,3 +1,4 @@
+import { list } from 'postcss';
 import React from 'react';
 
 const Modal = ({ singelData }) => {
@@ -8,20 +9,37 @@ const Modal = ({ singelData }) => {
         // You might want to return a default state or loading state
         return null;
     }
-    const { tool_name, image_link, description, integrations, features } = singelData.data;
-    console.log(tool_name);
+    const { image_link, description, integrations, features } = singelData.data;
+    console.log(integrations);
 
     return (
         <>
             <dialog id="my_modal_4" className="modal">
-                <div className="modal-box w-11/12 max-w-5xl">
-                    <div className="card lg:card-side bg-base-100">
-                        <figure><img  src={image_link && image_link[0]} alt="Album" /></figure>
-                        <div className="card-body">
+                <div className="modal-box w-11/12 max-w-5xl ">
+                    <div className="card lg:card-side bg-base-100 gap-5 ">
+                       
+                        <div  className="card-body border-2 border-orange-200 rounded-xl">
                             <h2 className="card-title">{description?description:'not found'}</h2>
-                            <p>Click the button to listen on Spotiwhy app.</p>                      
+                            <div className='flex justify-between'>
+                                <div>
+                                    <h1 className='text-xl font-bold'>Features</h1>
+                                    {
+                                        Object.values(features || {}).map(value => <li>{value.feature_name}</li>)
+                                    }
+                                </div>
+                                <div>
+                                    <h1 className='text-xl font-bold'>Integrations</h1>
+                                    
+                                       {
+                                        integrations && integrations.map((p) =><li>{p ? p : 'not found'}</li>)
+                                       }
+                                    
+                                </div>
+                            </div>                     
                         </div>
+                        <figure><img  src={image_link && image_link[0]} alt="Album" /></figure>
                     </div>
+
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button, it will close the modal */}
